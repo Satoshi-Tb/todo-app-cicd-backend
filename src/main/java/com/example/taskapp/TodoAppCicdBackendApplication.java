7a3fc6a0effef7c03781328b5dc7cdff5e013491
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-09-24 00:03:35 +0900

    1st commit

---
+++ src/main/java/com/example/taskapp/TodoAppCicdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppCicdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppCicdBackendApplication.class, args);
+	}
+
+}
